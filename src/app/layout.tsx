604d99059af3e9c34be393f63b532f5510654e80
Elkenzi <moelkenzi@gmail.com>
2025-05-02 23:08:33 +0300

    Add logo and sample text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,19 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [
+      { url: '/Soplang-01.png', media: '(prefers-color-scheme: light)' },
+    ]
+  }
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
